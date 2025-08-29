378b91a30def0f3d8674be4d02492587e94762d9
LouisClement <louisaadn@gmail.com>
2025-08-29 11:46:00 +0200

    feat: create a working launch script

--- server/start-servers.ps1
+++ server/start-servers.ps1
@@ -2,11 +2,11 @@
 Set-Location -Path $PSScriptRoot
 
 # Démarrer le serveur en arrière-plan
-# Start the server in the current window to see logs
- ts-node src/index.ts
+# Démarrer le serveur dans une nouvelle fenêtre pour voir les logs
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "npm start"
 
 # Attendre que le serveur démarre
 Start-Sleep -Seconds 2
 
 # Ouvrir la page web
-Start-Process "http://127.0.0.1:3000"
+Start-Process "http://127.0.0.1:3000/index-ws.html"
